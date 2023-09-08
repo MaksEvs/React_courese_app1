2cb34c3cbf9209091a98e06865dbd007f136a0a5
Maks <maksim.evseev666@gmail.com>
2023-09-08 14:01:43 +0300

    Create react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CostItem from './components/CostItem';
+
+function App() {
+    return (
+        <div>
+            <h1>Начнем изучение реакт</h1>
+            <CostItem></CostItem>
+        </div> 
+    );
+}
+
+export default App;
